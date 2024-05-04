7f4594428f5081779604057b0cf55ff83bf1a468
Rifat Mahmood <rftmahmood@gmail.com>
2024-05-05 00:05:36 +0600

    sd commit

--- db/queries.js
+++ db/queries.js
@@ -1,4 +1,5 @@
 import { recipeModel } from "@/models/recipe-modal";
+import dbConnect from "@/services/dbConnect";
 import { replaceMongoIdInArray, replaceMongoIdInObject } from "@/utils/data-util";
 import mongoose from "mongoose";
 
@@ -7,23 +8,27 @@ const { userModel } = require("@/models/user-model");
 
 
 async function createUser(user) {
+    ç
     return await userModel.create(user);
 }
 
 async function getAllRecipes() {
-
+    await dbConnect()
     const allRecipes = await recipeModel.find().lean();
+
     return replaceMongoIdInArray(allRecipes);
 }
 
 
 async function getRecipeById(recipeID) {
+    await dbConnect()
     const recipe = await recipeModel.findById(recipeID).lean();
     return replaceMongoIdInObject(recipe);
 }
 
 
 async function updateFav(recipeID, authID) {
+    await dbConnect()
     const user = await userModel.findById(authID);
 
     if (user) {
@@ -43,6 +48,7 @@ async function updateFav(recipeID, authID) {
 
 
 async function findUserByCredentials(credentials) {
+    await dbConnect()
     const user = await userModel.findOne(credentials).lean();
     if (user) {
         return replaceMongoIdInObject(user);
